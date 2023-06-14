033c42f5ab9e0f93aa193f2d029f1aa6a2ec2f72
kubgus <ja.gustafik@outlook.sk>
2023-06-14 14:24:17 +0200

    Test Z layer sorting

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -2,13 +2,89 @@
 
 namespace Sandbox
 {
-    class Dot : Root
+    class Whatever : Component
     {
-        public Dot()
+        public override void Draw()
         {
-            Transform.Scale = 4f;
+            if (Root.GetComponent<BoxCollider>().IsColliding("Player"))
+            {
+                Root.GetComponent<Quad>().Color = (255, 0, 33);
+            }
+            else
+            {
+                Root.GetComponent<Quad>().Color = (255, 100, 255);
+            }
 
-            AddComponent<Quad>().Color = (255, 20, 20);
+            if (Input.IsMouseButtonPressed(System.Windows.Forms.MouseButtons.Left)) { Disable(); }
+        }
+    }
+
+    class Box : Root
+    {
+        public Box()
+        {
+            Transform.Position = 300f;
+            Transform.Rotation = 50f;
+            Transform.Scale = 50f;
+
+            AddComponent<Quad>();
+            AddComponent<BoxCollider>().Tag = "Wall";
+            AddComponent<Whatever>();
+        }
+    }
+
+    class PlayerMovement : Component
+    {
+        public override void FixedUpdate()
+        {
+            PlayerData playerData = Root.GetComponent<PlayerData>();
+
+            Vector2 direction = 0f;
+
+            if (Input.IsKeyPressed(System.Windows.Forms.Keys.W)) { direction.Y = -1f; }
+            if (Input.IsKeyPressed(System.Windows.Forms.Keys.S)) { direction.Y = 1f; }
+            if (Input.IsKeyPressed(System.Windows.Forms.Keys.A)) { direction.X = -1f; }
+            if (Input.IsKeyPressed(System.Windows.Forms.Keys.D)) { direction.X = 1f; }
+
+            if (Input.IsKeyPressed(System.Windows.Forms.Keys.Z)) { Transform.Z = 1.7f; }
+            else { Transform.Z = 0f; }
+
+            Vector2 move = direction * playerData.Speed * Time.DeltaTime;
+            Transform.Position.X += move.X;
+            Transform.Position.Y += move.Y;
+            if (direction.X > 0f) { Transform.Rotation++; }
+            if (direction.X < 0f) { Transform.Rotation--; }
+        }
+    }
+
+    class PlayerData : Component
+    {
+        public float Speed;
+    }
+
+    class ColorScript : Component
+    {
+        public override void Update()
+        {
+            Root.GetComponent<Quad>().Color.R = (int)Transform.X / (512 / 255);
+            Root.GetComponent<Quad>().Color.G = (int)(512 - Transform.X) / (512 / 255);
+            Root.GetComponent<Quad>().Color.B = (int)Transform.Y / (512 / 255);
+        }
+    }
+
+    class Player : Root
+    {
+        public Player()
+        {
+            Transform.Position = 30f;
+            Transform.Scale = 30f;
+
+            AddComponent<Quad>().Color = (255, 0, 0);
+
+            AddComponent<PlayerData>().Speed = 50f;
+            AddComponent<PlayerMovement>();
+            AddComponent<ColorScript>();
+            AddComponent<BoxCollider>().Tag = "Player";
         }
     }
 
@@ -16,28 +92,13 @@ namespace Sandbox
     {
         public Game(Vector2 size, string title) : base(size, title) { }
 
-        List<Dot> dots;
+        public Player? Player;
+        public Box? Box;
 
         public override void Ready()
         {
-            Window.Renderer.ClearColor = 0;
-
-            dots = new List<Dot>();
-            for (int i = 0; i < 5000; i++)
-            {
-                Dot dot = new();
-                dots.Add(dot);
-            }
-        }
-
-        public override void Draw()
-        {
-            for (int i = 0; i < dots.Count; i++)
-            {
-                Dot dot = dots[i];
-                float time = Time.ElapsedMilliseconds / 500000f;
-                dot.Transform.Center = (i * (512f / dots.Count), MathF.Sin(time * i) * 100f + 256f);
-            }
+            Player = new Player();
+            Box = new Box();
         }
     }
 
